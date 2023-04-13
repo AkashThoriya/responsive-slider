296ff06a35530e2c60f6a48f1164e4f9dd14d980
akash <akash@fdna.com>
2023-04-13 15:57:53 +0530

    Add more detailed comments

--- slider.js
+++ slider.js
@@ -1,18 +1,18 @@
-// The code is wrapped in an Immediately Invoked Function Expression (IIFE) to avoid polluting the global namespace.
+// Wrap the code in an Immediately Invoked Function Expression (IIFE) to avoid polluting the global namespace.
 (function() {
-    // Get the slider element from the DOM using its ID.
+    // Get a reference to the slider element from the DOM using its ID.
     const slider = document.getElementById('slider');
-    // Get the child elements of the slider, which are the slides.
+    // Get the child elements of the slider, which represent the slides.
     const slides = slider.children;
 
-    // Declare the initial values for slideIndex, slideWidth, and slidesToShow.
+    // Initialize variables for the current slide index, slide width, and number of slides to show.
     let slideIndex = 0;
     let slideWidth = 0;
     let slidesToShow = 3;
 
     // Define a function to calculate the width of each slide based on the viewport width.
     const calculateSlideWidth = () => {
-        // Update the slidesToShow value based on the window.innerWidth value.
+        // Determine how many slides should be visible based on the current viewport width.
         if (window.innerWidth <= 767) {
             slidesToShow = 1;
         } else if (window.innerWidth <= 1024) {
@@ -20,14 +20,15 @@
         } else {
             slidesToShow = 3;
         }
-        // Calculate the slide width as a percentage of the viewport width.
+        // Calculate the width of each slide as a percentage based on the number of visible slides.
         slideWidth = 100 / slidesToShow;
     };
 
     // Define a function to clone the first 'slidesToShow' slides and append them to the slider.
+    // This is done to create an infinite loop effect when the slider reaches the end.
     function cloneSlides() {
         for (let i = 0; i < slidesToShow; i++) {
-            // Clone the slide and append it to the slider.
+            // Clone the current slide and append it to the slider.
             let clone = slides[i % slides.length].cloneNode(true);
             slider.appendChild(clone);
         }
@@ -35,14 +36,15 @@
 
     // Define a function to update the slide index based on the increment value.
     function updateSlideIndex(increment) {
-        // Update the slide index.
+        // Update the current slide index by adding the increment multiplied by the number of visible slides.
         slideIndex += increment * slidesToShow;
-        // If the slide index is out of bounds, reset it to 0 and update the slider position without a transition.
+        // Check if the slide index is out of bounds, meaning that the slider has reached the end.
         if (slideIndex >= slides.length - slidesToShow) {
+            // Reset the slide index to 0 and update the slider position without a transition.
             slideIndex = 0;
             slider.style.transition = "none";
             slider.style.transform = `translateX(-${slideIndex * slideWidth}%)`;
-            // Restore the transition after a brief delay.
+            // Restore the transition after a brief delay to ensure that the reset is not visible.
             setTimeout(() => {
                 slider.style.transition = "transform 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
             }, 50);
@@ -63,17 +65,17 @@
 
     // Define a function to update the slider position and slide width when the window is resized.
     function onResize() {
-        // Recalculate the slide width and update the slider position.
+        // Recalculate the slide width based on the new viewport width and update the slider position.
         calculateSlideWidth();
         slider.style.transform = `translateX(-${slideIndex * slideWidth}%)`;
     }
 
-    // Add an event listener to handle the window resize event.
+    // Add an event listener to handle the window resize event and adjust the slider accordingly.
     window.addEventListener('resize', onResize);
-    // Calculate the initial slide width.
+    // Calculate the initial slide width based on the viewport width.
     calculateSlideWidth();
-    // Clone the first 'slidesToShow' slides.
+    // Clone the first 'slidesToShow' slides to create an infinite loop effect.
     cloneSlides();
-    // Start auto-playing the slider.
+    // Start auto-playing the slider by moving to the next slide every 5 seconds.
     autoPlay();
-})();
+})();   
