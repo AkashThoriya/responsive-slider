bd40a14d0abda8a9a29314bb02aa0cfa9c787947
akash <akash@fdna.com>
2023-04-13 13:14:24 +0530

    add comments

--- slider.js
+++ slider.js
@@ -6,6 +6,7 @@
     let slideWidth = 0;
     let slidesToShow = 3;
 
+    // Calculate the width of each slide based on the viewport width
     const calculateSlideWidth = () => {
         if (window.innerWidth <= 767) {
             slidesToShow = 1;
@@ -17,6 +18,7 @@
         slideWidth = 100 / slidesToShow;
     };
 
+    // Clone the first 'slidesToShow' slides and append them to the slider
     function cloneSlides() {
         for (let i = 0; i < slidesToShow; i++) {
             let clone = slides[i % slides.length].cloneNode(true);
@@ -24,6 +26,7 @@
         }
     }
 
+    // Update the slide index based on the increment value
     function updateSlideIndex(increment) {
         slideIndex += increment * slidesToShow;
         if (slideIndex >= slides.length - slidesToShow) {
@@ -36,20 +39,24 @@
         }
     }
 
+    // Move the slider to the next slide
     function nextSlide() {
         updateSlideIndex(1);
         slider.style.transform = `translateX(-${slideIndex * slideWidth}%)`;
     }
 
+    // Auto-play the slider by moving to the next slide every 5 seconds
     function autoPlay() {
-        setInterval(nextSlide, 2000);
+        setInterval(nextSlide, 5000);
     }
 
+    // Update the slider position and slide width when the window is resized
     function onResize() {
         calculateSlideWidth();
         slider.style.transform = `translateX(-${slideIndex * slideWidth}%)`;
     }
 
+    // Add event listeners and initialize the slider
     window.addEventListener('resize', onResize);
     calculateSlideWidth();
     cloneSlides();
